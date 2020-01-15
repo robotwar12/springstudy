91712b0ec9192a92f58c715b3b7009f9bba0a061
kim jung min <robotwar12@naver.com>
2020-01-15 10:02:25 +0900

    Oauth properties add

--- src/main/java/com/kjm/study/spring/web/HelloController.java
+++ src/main/java/com/kjm/study/spring/web/HelloController.java
@@ -16,5 +16,6 @@ public class HelloController {
     @GetMapping("/hello/dto")
     public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
         return new HelloResponseDto(name,amount);
+
     }
 }
